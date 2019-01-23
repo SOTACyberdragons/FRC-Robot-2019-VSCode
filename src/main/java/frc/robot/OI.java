d06f77655947cfcbd810e49eff62ab787545e689
Renata Volchinskaya <rvolchinskaya@yahoo.com>
2019-01-22 18:35:01 -0800

    working solenoid code + check in gradle artifacts + code formatting

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -7,34 +7,40 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import frc.robot.commands.PistonIn;
+import frc.robot.commands.PistonOut;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-public XboxController controller = new XboxController(0);
+    public XboxController controller = new XboxController(0);
+    JoystickButton pistonOut;
+    JoystickButton pistonIn;
+    // Setting squaredInput to true decreases the sensitivity for tankdrive at lower speeds
+    private boolean squaredInput = true;
 
-	
-	// Setting squaredInput to true decreases the sensitivity for tankdrive at lower speeds
-	private boolean squaredInput = true;
 
-	
-	
-	public OI() {
-			
-		
-	}
+    public OI() {
+        pistonOut = new JoystickButton(controller, 1);
+        pistonOut.whenPressed(new PistonOut());
+        pistonIn = new JoystickButton(controller, 2);
+        pistonIn.whenPressed(new PistonIn());
+    }
 
-	
-	public XboxController.Thumbstick getLeftStick() {
-		return controller.leftStick;	
-	}
-	
-	public XboxController.Thumbstick getRightStick() {
-		return controller.rightStick;
-	}
-	public boolean getSquaredInput() {
-		return squaredInput;
-	}
+
+    public XboxController.Thumbstick getLeftStick() {
+        return controller.leftStick;
+    }
+
+    public XboxController.Thumbstick getRightStick() {
+        return controller.rightStick;
+    }
+
+    public boolean getSquaredInput() {
+        return squaredInput;
+    }
 }
 
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,14 +8,14 @@
 
 package frc.robot;
 
-import frc.robot.subsystems.DriveTrain;
-
-import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Preferences;
+import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.Prototype;
 
 
 /**
@@ -26,100 +26,99 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * project.
  */
 public class Robot extends TimedRobot {
-	private Command autonomousCommand;
-	public static Preferences prefs;
-	SendableChooser<Command> chooser = new SendableChooser<>();
+    public static Preferences prefs;
+    public static DriveTrain drivetrain;
+    public static Prototype prototype;
+    public static OI oi;
+    SendableChooser<Command> chooser;
+    private Command autonomousCommand;
 
-	
-	public static OI oi;
-	public static DriveTrain drivetrain;
+    @Override
+    public void robotInit() {
+        drivetrain = new DriveTrain();
+        prototype = new Prototype();
+        oi = new OI();
 
-	
-	@Override
-	public void robotInit() {
-		drivetrain = new DriveTrain();
-		oi = new OI();
-		
-		
-		
-		
-		SmartDashboard.putData("Auto mode", chooser);
-	}
+        chooser = new SendableChooser<>();
 
-	/**
-	 * This function is called once each time the robot enters Disabled mode.
-	 * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
-	 */
-	@Override
-	public void disabledInit() {
+        SmartDashboard.putData("Auto mode", chooser);
+    }
 
-	}
+    /**
+     * This function is called once each time the robot enters Disabled mode.
+     * You can use it to reset any subsystem information you want to clear when
+     * the robot is disabled.
+     */
+    @Override
+    public void disabledInit() {
 
-	@Override
-	public void disabledPeriodic() {
-		Scheduler.getInstance().run();
-	}
+    }
 
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString code to get the auto name from the text box below the Gyro
-	 *
-	 * <p>You can add additional auto modes by adding additional commands to the
-	 * chooser code above (like the commented example) or additional comparisons
-	 * to the switch structure below with additional strings & commands.
-	 */
-	@Override
-	public void autonomousInit() {
-		autonomousCommand = chooser.getSelected();
+    @Override
+    public void disabledPeriodic() {
+        Scheduler.getInstance().run();
+    }
 
-		/*
-		 * String autoSelected = SmartDashboard.getString("Auto Selector",
-		 * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
-		 * = new MyAutoCommand(); break; case "Default Auto": default:
-		 * autonomousCommand = new ExampleCommand(); break; }
-		 */
+    /**
+     * This autonomous (along with the chooser code above) shows how to select
+     * between different autonomous modes using the dashboard. The sendable
+     * chooser code works with the Java SmartDashboard. If you prefer the
+     * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+     * getString code to get the auto name from the text box below the Gyro
+     *
+     * <p>You can add additional auto modes by adding additional commands to the
+     * chooser code above (like the commented example) or additional comparisons
+     * to the switch structure below with additional strings & commands.
+     */
+    @Override
+    public void autonomousInit() {
+//		autonomousCommand = chooser.getSelected();
 
-		// schedule the autonomous command (example)
-		if (autonomousCommand != null) {
-			autonomousCommand.start();
-		}
-	}
+        /*
+         * String autoSelected = SmartDashboard.getString("Auto Selector",
+         * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
+         * = new MyAutoCommand(); break; case "Default Auto": default:
+         * autonomousCommand = new ExampleCommand(); break; }
+         */
 
-	/**
-	 * This function is called periodically during autonomous.
-	 */
-	@Override
-	public void autonomousPeriodic() {
-		Scheduler.getInstance().run();
-	}
+        //schedule the autonomous command (example)
+        if (autonomousCommand != null) {
+            autonomousCommand.start();
+        }
+    }
 
-	@Override
-	public void teleopInit() {
-		// This makes sure that the autonomous stops running when
-		// teleop starts running. If you want the autonomous to
-		// continue until interrupted by another command, remove
-		// this line or comment it out.
-		if (autonomousCommand != null) {
-			autonomousCommand.cancel();
-		}
-	}
+    /**
+     * This function is called periodically during autonomous.
+     */
+    @Override
+    public void autonomousPeriodic() {
+        Scheduler.getInstance().run();
+    }
 
-	/**
-	 * This function is called periodically during operator control.
-	 */
-	@Override
-	public void teleopPeriodic() {
-		Scheduler.getInstance().run();
-	}
+    @Override
+    public void teleopInit() {
+        // This makes sure that the autonomous stops running when
+        // teleop starts running. If you want the autonomous to
+        // continue until interrupted by another command, remove
+        // this line or comment it out.
+        if (autonomousCommand != null) {
+            autonomousCommand.cancel();
+        }
 
-	/**
-	 * This function is called periodically during test mode.
-	 */
-	@Override
-	public void testPeriodic() {
-	}
+    }
+
+    /**
+     * This function is called periodically during operator control.
+     */
+    @Override
+    public void teleopPeriodic() {
+        Scheduler.getInstance().run();
+    }
+
+    /**
+     * This function is called periodically during test mode.
+     */
+    @Override
+    public void testPeriodic() {
+    }
 }
