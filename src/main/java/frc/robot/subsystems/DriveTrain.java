396df7f1224f54ff1a6bec8e415752444165c5dc
Renata Volchinskaya <rvolchinskaya@yahoo.com>
2019-01-31 18:58:49 -0800

    adds naomi's talon code

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,10 +1,15 @@
-package frc.robot.subsystems;
+package org.usfirst.frc.team5700.robot.subsystems;
+
+import org.usfirst.frc.team5700.robot.Robot;
+import org.usfirst.frc.team5700.robot.RobotMap;
+import org.usfirst.frc.team5700.robot.commands.DifferentialDriveWithXbox;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import frc.robot.RobotMap;
-import frc.robot.commands.DifferentialDriveWithXbox;
 
 
 /**
@@ -12,27 +17,44 @@ import frc.robot.commands.DifferentialDriveWithXbox;
  */
 public class DriveTrain extends Subsystem {
 
+	
+	private TalonSRX leftMotor;
+	private TalonSRX leftMotorFollower;
+	private TalonSRX rightMotor;
+	private TalonSRX rightMotorFollower;
 
-    public DifferentialDrive drive;
-    private Spark leftMotor;
-    private Spark rightMotor;
+	public DifferentialDrive drive; 
+	
+	
+	public DriveTrain() {
 
+		leftMotor = new TalonSRX(RobotMap.LEFT_DRIVE_MOTOR);
+		rightMotor = new TalonSRX(RobotMap.RIGHT_DRIVE_MOTOR);
+		leftMotorFollower = new TalonSRX(RobotMap.LEFT_DRIVE_FOLLOW_MOTOR);
+		rightMotorFollower = new TalonSRX(RobotMap.RIGHT_DRIVE_FOLLOW_MOTOR);
+	
+		
+		Robot.initTalon(leftMotor);
+		Robot.initTalon(rightMotor);
+		Robot.initTalon(rightMotorFollower);
+		Robot.initTalon(leftMotorFollower);
+		
+		leftMotorFollower.follow(leftMotor);
+		rightMotorFollower.follow(rightMotor);
+		
+		rightMotor.setInverted (true);
+		
+	}
+	public void set(ControlMode mode, double leftvalue, double rightvalue) {
+		leftMotor.set(mode,leftvalue);
+		rightMotor.set(mode,leftvalue);
 
-    public DriveTrain() {
-
-        leftMotor = new Spark(RobotMap.LEFT_DRIVE_MOTOR);
-        rightMotor = new Spark(RobotMap.RIGHT_DRIVE_MOTOR);
-        drive = new DifferentialDrive(leftMotor, rightMotor);
-    }
-
-    public void stop() {
-        drive.curvatureDrive(0.0, 0.0, true);
-    }
-
-    public void initDefaultCommand() {
-        setDefaultCommand(new DifferentialDriveWithXbox());
-    }
+	}
+	public void stop() {
+		drive.curvatureDrive(0.0, 0.0, true);
+	}
+	
+	public void initDefaultCommand() {
+		setDefaultCommand(new DifferentialDriveWithXbox());
+	}
 }
-
-
-
