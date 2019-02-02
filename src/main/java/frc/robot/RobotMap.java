98661b6acb9afcac9ea252d174cdf7bef37b8dc6
Dmitry <dmitry.dimov@gmail.com>
2019-02-02 13:30:33 -0800

    add stuff to drivetrain class

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -15,10 +15,10 @@ package frc.robot;
  */
 public class RobotMap {
 	//PWM
-	public static final int LEFT_DRIVE_MOTOR = 0;
-	public static final int RIGHT_DRIVE_MOTOR = 14;
-	public static final int LEFT_DRIVE_FOLLOW_MOTOR = 1;
-	public static final int RIGHT_DRIVE_FOLLOW_MOTOR = 15;
+	public static final int LEFT_DRIVE_MOTOR = 10;
+	public static final int RIGHT_DRIVE_MOTOR = 4;
+	public static final int LEFT_DRIVE_FOLLOW_MOTOR = 8;
+	public static final int RIGHT_DRIVE_FOLLOW_MOTOR = 7;
 	public static final int ARM_MOTOR = 2;
 
 }
--- src/main/java/frc/robot/commands/DifferentialDriveWithJoysticks.java
+++ src/main/java/frc/robot/commands/DifferentialDriveWithJoysticks.java
@@ -25,8 +25,8 @@ public class DifferentialDriveWithJoysticks extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
         double throttle = 0.8;
-        Robot.drivetrain.set(ControlMode.PercentOutput, Robot.oi.getLeftStick().getY()*throttle,
-		Robot.oi.getRightStick().getX()*throttle);
+        Robot.drivetrain.drive.arcadeDrive(Robot.oi.getLeftStick().getY()*throttle,
+		Robot.oi.getRightStick().getX()*throttle, true);
     }
 
     // Make this return true when this Command no longer needs to run execute()
